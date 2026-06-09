560d2196ffe99ee1c1d08bd3fedcc5226d67da65
Kavya Agrawal <kaaaavvvvssss@Kavyas-MacBook-Pro.local>
2026-06-09 16:20:58 +0530

    debug api request body

--- api/contact.js
+++ api/contact.js
@@ -49,5 +49,16 @@ export default async function handler(req, res) {
     });
 
   }
+  
+}
+export default async function handler(req, res) {
+  console.log("BODY:", req.body);
 
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
+  return res.status(200).json({
+    received: req.body
+  });
 }
